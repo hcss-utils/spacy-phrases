9393cff9f14723156ca596ab68eac27cac3efe4a
hp0404 <hryhorii.pavlenko@gmail.com>
2021-11-07 00:29:22 +0200

    replaced pandas with csv

--- scripts/noun_chunks.py
+++ scripts/noun_chunks.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 """Typer app that extracts noun phrases using spaCy's noun_chunks."""
+import csv
 import json
 from collections import defaultdict
 from pathlib import Path
-from typing import Dict, Iterator, List
+from typing import Dict, Tuple, Iterator, List
 
-import pandas as pd  # type: ignore
 import spacy
 import typer
 
+DataTuple = Tuple[str, str]
 Phrases = Dict[str, List[str]]
 
 
@@ -19,12 +20,19 @@ def update_jsonl(path: Path, lines: Phrases) -> None:
         output.write("\n")
 
 
+def build_tuples(path: Path, uuid: str, text: str) -> Iterator[DataTuple]:
+    """Build data tuples (text, identifier) for spaCy's pipes."""
+    with path.open("r", newline="", encoding="utf-8") as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=",")
+        for row in csv_reader:
+            yield row[text], row[uuid]
+
+
 def extract_chunks(
     nlp: spacy.language.Language,
-    csv_reader: pd.io.parsers.TextFileReader,
-    text: str = "fulltext",
-    uuid: str = "uuid",
-    pattern: str = "influenc",
+    data_tuples: Iterator[DataTuple],
+    batch_size: int,
+    pattern: str,
 ) -> Iterator[Phrases]:
     """Extract noun-chunks from streamed 'csv_reader'.
 
@@ -32,54 +40,46 @@ def extract_chunks(
     ----------
     nlp: spacy.language.Language
         language model that identifies phrases and takes care of lemmatization
-    csv_reader: pd.io.parsers.TextFileReader
-        pandas' file reader that processes .csv file in chunks
-    text: str
-        text column that we extract phrases from (stored as dict values)
-    uuid: str
-        id column that we use to identify phrases (stored as dict keys)
+    data_tuples: DataTuple
+        tuple of text and its identifier
+    batch_size: int
+        the number of texts to buffer
     pattern: str
-        regex pattern that identifies relevant texts
+        noun that identifies relevant noun chunks
     """
-    for chunk in csv_reader:
-        mask = chunk[text].str.contains(pattern, na=False, case=False)
-        df = chunk.loc[mask].reset_index(drop=True)
-        if df.empty:
-            continue
-        data_tuples = ((df.loc[idx, text], df.loc[idx, uuid]) for idx in df.index)
-        for doc, _id in nlp.pipe(data_tuples, as_tuples=True):
-            phrases = defaultdict(list)
-            for noun_chunk in doc.noun_chunks:
-                if (
-                    pattern not in noun_chunk.lemma_
-                    or any(t.is_stop or t.is_digit for t in noun_chunk)
-                    or len(noun_chunk) < 2
-                ):
-                    continue
-                phrases[_id].append(noun_chunk.lemma_.lower())
-            if phrases:
-                yield phrases
+    for doc, _id in nlp.pipe(data_tuples, as_tuples=True, batch_size=batch_size):
+        phrases = defaultdict(list)
+        for noun_chunk in doc.noun_chunks:
+            if (
+                pattern not in noun_chunk.lemma_
+                or any(t.is_stop or t.is_digit for t in noun_chunk)
+                or len(noun_chunk) < 2
+            ):
+                continue
+            phrases[_id].append(noun_chunk.lemma_.lower())
+        if phrases:
+            yield phrases
 
 
 def main(
     input_table: Path = typer.Argument(..., exists=True, dir_okay=False),
     output_jsonl: Path = typer.Argument(..., dir_okay=False),
     model: str = "en_core_web_sm",
-    models_max_length: int = 2_000_000,
-    table_chunksize: int = 10,
+    docs_max_length: int = 2_000_000,
+    batch_size: int = 50,
     text_field: str = "fulltext",
     uuid_field: str = "uuid",
     pattern: str = "influenc",
 ) -> None:
     """Extract noun phrases using spaCy."""
     nlp = spacy.load(model, disable=["ner"])
-    nlp.max_length = models_max_length
-    csv_reader = pd.read_csv(input_table, chunksize=table_chunksize)
+    nlp.max_length = docs_max_length
+    csv.field_size_limit(docs_max_length)
+    data_tuples = build_tuples(input_table, uuid=uuid_field, text=text_field)
     for document in extract_chunks(
         nlp=nlp,
-        csv_reader=csv_reader,
-        text=text_field,
-        uuid=uuid_field,
+        data_tuples=data_tuples,
+        batch_size=batch_size,
         pattern=pattern,
     ):
         update_jsonl(output_jsonl, document)
