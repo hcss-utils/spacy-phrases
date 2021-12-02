6efe9dc19c290e00a422cb72d317effcf3105a60
hp0404 <hryhorii.pavlenko@gmail.com>
2021-12-02 18:05:33 +0200

    adjusted processing script for #20 (multiple changes)

--- scripts/process.py
+++ scripts/process.py
@@ -2,13 +2,13 @@
 """Text preprocessing."""
 import csv
 import json
-from pathlib import Path
 from functools import partial
-from typing import Iterator, Dict, Union, Tuple, List
+from pathlib import Path
+from typing import Dict, Iterator, List, Set, Tuple, Union
 
-import typer
 import spacy
-from textacy import preprocessing, extract  # type: ignore
+import typer
+from textacy import extract, preprocessing  # type: ignore
 
 DataTuple = Tuple[str, str]
 ProcessedDoc = Dict[str, Union[str, List[str], List[Dict[str, List[str]]]]]
@@ -30,6 +30,18 @@ preproc = preprocessing.make_pipeline(
 )
 
 
+def read_processed_data(path: Path) -> Set[str]:
+    """Collect processed uuids."""
+    seen: Set[str] = set()
+    if not path.exists():
+        return seen
+    with path.open("r", newline="", encoding="utf-8") as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=",")
+        for row in csv_reader:
+            seen.add(row["document_id"])
+    return seen
+
+
 def create_nlp(model: str, max_length: int) -> spacy.language.Language:
     """Customize spaCy's built-in loader."""
     nlp = spacy.load(model)
@@ -44,12 +56,33 @@ def update_jsonl(path: Path, lines: ProcessedDoc) -> None:
         output.write("\n")
 
 
-def build_tuples(path: Path, uuid: str, text: str) -> Iterator[DataTuple]:
+def build_tuples(
+    path: Path, uuid: str, text: str, processed_uuids: Set[str]
+) -> Iterator[DataTuple]:
     """Builds data tuples (text, identifier) for spaCy's pipes."""
     with path.open("r", newline="", encoding="utf-8") as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=",")
         for row in csv_reader:
-            yield row[text], row[uuid]
+            if row[uuid] in processed_uuids:
+                continue
+            yield row[text], (row[uuid], row["unit_id"])
+
+
+def write_csv(path: Path, data: ProcessedDoc, write_header: bool = False) -> None:
+    """Write processed data to .csv file, in chunks."""
+    with path.open("a", newline="", encoding="utf-8") as csv_file:
+        fieldnames = [
+            "document_id",
+            "unit_id",
+            "fulltext",
+            "lemmas",
+            "terms",
+            "subject_verb_object_triples",
+        ]
+        writer = csv.DictWriter(csv_file, delimiter=",", fieldnames=fieldnames)
+        if write_header:
+            writer.writeheader()
+        writer.writerow(data)
 
 
 def process(
@@ -62,10 +95,10 @@ def process(
     sovs: bool,
 ) -> Iterator[ProcessedDoc]:
     """Preprocessing text."""
-    for doc, document_id in nlp.pipe(data, as_tuples=True, batch_size=batch_size):
-        record: ProcessedDoc = {"document_id": document_id}
+    for doc, (document_id, unit_id) in nlp.pipe(data, as_tuples=True, batch_size=batch_size):
+        record: ProcessedDoc = {"document_id": document_id, "unit_id": unit_id}
         if preprocess:
-            record["processed_text"] = preproc(doc.text)
+            record["fulltext"] = preproc(doc.text)
         if lemmas:
             record["lemmas"] = [t.lemma_ for t in doc if t.is_alpha and not t.is_stop]
         if terms:
@@ -76,8 +109,7 @@ def process(
                     ents=partial(
                         extract.entities, include_types=["PERSON", "ORG", "GPE"]
                     ),
-                    ncs=True,
-                    ngs=lambda doc: extract.ngrams(doc, n=(2, 3)),
+                    ngs=lambda doc: extract.ngrams(doc, n=(1, 3)),
                 )
             ]
         if sovs:
@@ -96,8 +128,8 @@ def main(
     input_table: Path = typer.Argument(
         ..., exists=True, dir_okay=False, help="Input table containing text & metadata"
     ),
-    output_jsonl: Path = typer.Argument(
-        ..., dir_okay=False, help="Output JSONLines file where matches will be stored"
+    output_table: Path = typer.Argument(
+        ..., dir_okay=False, help="Output csv file where matches will be stored"
     ),
     model: str = typer.Option("en_core_web_sm", help="SpaCy model's name"),
     docs_max_length: int = typer.Option(2_000_000, help="Doc's max length."),
@@ -114,8 +146,9 @@ def main(
         raise ValueError("You should call at least 1 pipeline.")
     csv.field_size_limit(docs_max_length)
     nlp = create_nlp(model, docs_max_length)
-    data_tuples = build_tuples(input_table, uuid_field, text_field)
-    for doc in process(
+    processed_uuids = read_processed_data(output_table)
+    data_tuples = build_tuples(input_table, uuid_field, text_field, processed_uuids)
+    transformer = process(
         nlp=nlp,
         data=data_tuples,
         batch_size=batch_size,
@@ -123,8 +156,10 @@ def main(
         lemmas=lemmas,
         terms=terms,
         sovs=sovs,
-    ):
-        update_jsonl(output_jsonl, doc)
+    )
+    for idx, doc in enumerate(transformer):
+        write_header = bool(idx == 0 and not processed_uuids)
+        write_csv(output_table, doc, write_header)
 
 
 if __name__ == "__main__":
